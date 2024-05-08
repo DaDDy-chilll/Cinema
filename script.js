ff2dc0990cbfe89faeb4e7a1c25154abfc68a6d3
DaDDy <blacklistuser420@gmail.com>
2024-05-08 15:44:20 +0630

    update

--- script.js
+++ script.js
@@ -11,16 +11,31 @@ const json = {
   totalSeats: 2,
   seats: [
     { seatCode: "B4", seatPrice: 5500 },
-    { seatCode: "B5", seatPrice: 5500 },
+    { seatCode: "B5", seatPrice: 5400 },
   ],
 };
 
 
 
-$('#movieName').text(json.movie)
-$('#showDate').text(json.showDate)
-$('#showTime').text(json.showTime)
-$('#cinema').text(json.cinema)
+const {cinema,movie,poster,showDate,showTime,foodItems,totalSeats,seats} = json;
+const totalFoodPrice = foodItems.reduce((acc,item) => acc + (item.price * item.quantity),0); //!ask to hste paing
+const totalSeatPrice = seats.reduce((acc,item) => acc + item.seatPrice,0);
+const totalPrice = totalFoodPrice + totalSeatPrice;
+const tax = 10
+const addTaxPrice = parseInt(totalPrice * tax /100);
+const totalPriceWithTax = totalPrice + addTaxPrice;
+
+(() => {
+  $('#movieName').text(movie);
+  $('#showDate').text(showDate);
+  $('#showTime').text(showTime);
+  $('#cinema').text(cinema);
+  $('#totalSeats').text(totalSeats);
+  $('#ticketPrice').text(`${totalSeatPrice} Ks`);
+  $('#foodPrice').text(`${totalFoodPrice} Ks`);
+  $('#tax').text(`${tax}%`);
+  $('#totalPrice').text(`${totalPriceWithTax} Ks`);
+})()
 
 
 $("#paymentCloseBtn").click(() => {
@@ -36,48 +51,56 @@ $("#mobilePay").click(() => {
     alt="cbpay"
     width="12.6%"
     class='mobile'
+    id='mobilePayImage'
   />
   <img
     src="assets/payment-provider/mobile/kbzpay.png"
     alt="kpay"
     width="12.6%"
     class='mobile'
+    id='mobilePayImage'
   />
   <img
     src="assets/payment-provider/mobile/okpay.png"
     alt="okpay"
     width="12.6%"
     class='mobile'
+    id='mobilePayImage'
   />
   <img
     src="assets/payment-provider/mobile/ayapay.png"
     alt="ayapay"
     width="25%"
     class='mobile'
+    id='mobilePayImage'
   />
   <img
     src="assets/payment-provider/mobile/mytelpay.png"
     alt="mytelpay"
     width="25%"
     class='mobile'
+    id='mobilePayImage'
   />
   <img
     src="assets/payment-provider/mobile/onepay.png"
     alt="onepay"
     width="25%"
     class='mobile'
+    id='mobilePayImage'
   />
   <img
     src="assets/payment-provider/mobile/truemoney.png"
     alt="truepay"
     width="25%"
     class='mobile'
+    id='mobilePayImage'
   />
   <img
     src="assets/payment-provider/mobile/wavepay.png"
     alt="wavepay"
     width="25%"
     class='mobile'
+    id='mobilePayImage'
   />
     `;
 
@@ -88,14 +111,23 @@ $("#mobilePay").click(() => {
 
 $("#bankPay").click(() => {
   const images = `
-    <img src="assets/payment-provider/bank/abank.png" alt="abank" width="25%" class="bank"/>
-    <img src="assets/payment-provider/bank/kbz.png" alt="kbz" width="25%" class="bank"/>
-    <img src="assets/payment-provider/bank/mab.png" alt="mab" width="25%" class="bank"/>
-    <img src="assets/payment-provider/bank/uab.png" alt="uab" width="25%" class="bank"/>
-    <img src="assets/payment-provider/bank/cb.png" alt="cb" width="25%" class="bank"/>
+    <img src="assets/payment-provider/bank/abank.png" alt="abank" width="25%" class="bank" id='bankPayImage' onClick='clickEvent(this)'/>
+    <img src="assets/payment-provider/bank/kbz.png" alt="kbz" width="25%" class="bank" id='bankPayImage' onClick='clickEvent(this)'/>
+    <img src="assets/payment-provider/bank/mab.png" alt="mab" width="25%" class="bank" id='bankPayImage' onClick='clickEvent(this)'/>
+    <img src="assets/payment-provider/bank/uab.png" alt="uab" width="25%" class="bank" id='bankPayImage' onClick='clickEvent(this)'/>
+    <img src="assets/payment-provider/bank/cb.png" alt="cb" width="25%" class="bank" id='bankPayImage' onClick='clickEvent(this)'/>
     `;
 
   if ($("#providersContainer").html().length === 0)
     $("#providersContainer").append(images);
   $("#paymentProvider").fadeIn();
 });
+
+
+$('#providersContainer').children('img').click(() => {
+  console.log('mobilePay');
+})
+
+const clickEvent = (element) => {
+  console.log(element)
+}
